1ec78ef24b87ea547d5b29a169333f4b86e2c6fe
Daniella Cardona <danicp27@gmail.com>
2024-01-04 00:26:56 +0100

    conoce-a page v1 with general extra features

--- src/app/components/button/button.js
+++ src/app/components/button/button.js
@@ -2,9 +2,13 @@ import Button from 'react-bootstrap/Button';
 
 import styles from './button.module.css';
 
-export default function MyButton({ buttonText }){
+export default function MyButton({ buttonText, padding }){
+    const dynamicStyles = {
+        padding,
+      };
+
     return (    
-        <Button className={`latoBold ${styles.button}`} href="https://www.doctorwho.tv/" target="_blank">
+        <Button className={`latoBold ${styles.button}`} href="https://www.doctorwho.tv/" target="_blank" style={{ ...dynamicStyles }}>
             { buttonText }
         </Button>
     );
